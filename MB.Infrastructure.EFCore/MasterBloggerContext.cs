daa10cb227637c186ad19564ff09a2bbafcd6cfd
Shaqa-va <shaqavaziri1@gmail.com>
2021-12-12 00:02:48 +0330

    Article Domain and Mappings  Implemented and Repository & Application structure created

--- MB.Infrastructure.EFCore/MasterBloggerContext.cs
+++ MB.Infrastructure.EFCore/MasterBloggerContext.cs
@@ -1,4 +1,5 @@
-﻿using MB.Domain.ArticleCategoryAgg;
+﻿using MB.Domain.ArticleAgg;
+using MB.Domain.ArticleCategoryAgg;
 using MB.Infrastructure.EFCore.Mapping;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,6 +12,7 @@ namespace MB.Infrastructure.EFCore
     {
 
         public DbSet<ArticleCategory> ArticleCategories { get; set; }
+        public DbSet<Article> Articles { get; set; }
         public MasterBloggerContext(DbContextOptions<MasterBloggerContext> options):base(options)
         {
 
@@ -19,6 +21,7 @@ namespace MB.Infrastructure.EFCore
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ArticleCategoryMapping());
+            modelBuilder.ApplyConfiguration(new ArticleMapping());
             base.OnModelCreating(modelBuilder);
         }
     }
